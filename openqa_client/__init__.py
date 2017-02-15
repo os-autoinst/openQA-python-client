72f57aa3e7d45f3c11f74cede6515e10c843d99a
Adam Williamson <awilliam@redhat.com>
2017-02-15 12:47:08 -0800

    Add version to __init__.py and add a release script

---
+++ openqa_client/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (C) Red Hat Inc.
+#
+# openqa_client is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author:   Adam Williamson <awilliam@redhat.com>
+
+"""Python client library for openQA."""
+
+from __future__ import unicode_literals
+from __future__ import print_function
+
+__version__ = "1.2"
---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+baddeps=""
+# check deps
+rpm -qi python2-setuptools > /dev/null 2>&1 || baddeps="python2-setuptools"
+rpm -qi python2-setuptools_git > /dev/null 2>&1 || baddeps="${baddeps} python2-setuptools_git"
+if [ -n "${baddeps}" ]; then
+    echo "${baddeps} must be installed!"
+    exit 1
+fi
+
+if [ "$#" != "1" ]; then
+    echo "Must pass release version!"
+    exit 1
+fi
+
+version=$1
+name=openqa_client
+sed -i -e "s,version = \".*\",version = \"$version\", g" setup.py
+sed -i -e "s,__version__ = \".*\",__version__ = \"${version}\", g" ${name}/__init__.py
+git add setup.py ${name}/__init__.py
+git commit -s -m "Release $version"
+git push
+git tag -a -m "Release $version" $version
+git push origin $version
+python ./setup.py sdist --formats=tar
+xz dist/$name-$version.tar
