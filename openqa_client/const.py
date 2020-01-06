17384836653ff7e9dd5ff9a7cfb18ba16388a301
Adam Williamson <awilliam@redhat.com>
2020-01-06 12:20:51 -0800

    **API**: update constants to match upstream 4d89041
    Upstream changed the job constants a bit, so we must change to
    follow them. The most notable change is that the
    JOB_INCOMPLETE_RESULTS tuple is gone. JOB_NOT_COMPLETE_RESULTS
    is *NOT* the same, so do not use it as a replacement. However,
    JOB_COMPLETE_RESULTS is the exact inverse, so you can trivially
    change a check for something being in JOB_NOT_COMPLETE_RESULTS
    to a check for it not being in JOB_COMPLETE_RESULTS, for e.g.
    
    Aside from that, new constants were added:
    
    * JOB_NOT_COMPLETE_RESULTS
    * JOB_ABORTED_RESULTS
    * JOB_NOT_OK_RESULTS
    
    and these new 'meta' states and results:
    
    * JOB_STATE_PRE_EXECUTION
    * JOB_STATE_EXECUTION
    * JOB_STATE_FINAL
    
    * JOB_RESULT_COMPLETE
    * JOB_RESULT_NOT_COMPLETE
    * JOB_RESULT_ABORTED
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- openqa_client/const.py
+++ openqa_client/const.py
@@ -21,7 +21,9 @@ rather than every consumer of this library duplicating things like
 'these are the "running" states' on the fly. It is explicitly allowed
 to use 'from openqa_client.const import *'; this will only import
 sanely named 'constants'. You may prefer to do `from openqa_client
-import const as oqc` or similar.
+import const as oqc` or similar. For details on what each of these
+means, please refer to the openQA source, it has comments that explain
+them; it seems unnecessary to duplicate those here.
 """
 
 # we use 'bad' whitespace to align the definitions nicely.
@@ -31,12 +33,12 @@ import const as oqc` or similar.
 
 # States
 JOB_STATE_SCHEDULED = "scheduled"
+JOB_STATE_ASSIGNED =  "assigned"
 JOB_STATE_SETUP =     "setup"
 JOB_STATE_RUNNING =   "running"
+JOB_STATE_UPLOADING = "uploading"
 JOB_STATE_CANCELLED = "cancelled"
 JOB_STATE_DONE =      "done"
-JOB_STATE_UPLOADING = "uploading"
-JOB_STATE_ASSIGNED =  "assigned"
 
 JOB_STATES =               (JOB_STATE_SCHEDULED, JOB_STATE_SETUP, JOB_STATE_RUNNING,
                             JOB_STATE_CANCELLED, JOB_STATE_DONE, JOB_STATE_UPLOADING,
@@ -47,6 +49,11 @@ JOB_EXECUTION_STATES =     (JOB_STATE_ASSIGNED, JOB_STATE_SETUP,
                             JOB_STATE_RUNNING, JOB_STATE_UPLOADING)
 JOB_PRE_EXECUTION_STATES = (JOB_STATE_SCHEDULED,)
 JOB_FINAL_STATES =         (JOB_STATE_DONE, JOB_STATE_CANCELLED)
+
+# These are referred to as 'meta' states upstream
+JOB_STATE_PRE_EXECUTION = "pre_execution"
+JOB_STATE_EXECUTION =     "execution"
+JOB_STATE_FINAL =         "final"
 
 # Results
 JOB_RESULT_NONE =               "none"
@@ -62,17 +69,23 @@ JOB_RESULT_USER_CANCELLED =     "user_cancelled"
 JOB_RESULT_USER_RESTARTED =     "user_restarted"
 JOB_RESULT_TIMEOUT_EXCEEDED =   "timeout_exceeded"
 
-JOB_RESULTS =            (JOB_RESULT_NONE, JOB_RESULT_PASSED, JOB_RESULT_SOFTFAILED,
-                          JOB_RESULT_FAILED, JOB_RESULT_INCOMPLETE, JOB_RESULT_SKIPPED,
-                          JOB_RESULT_OBSOLETED, JOB_RESULT_PARALLEL_FAILED,
-                          JOB_RESULT_PARALLEL_RESTARTED, JOB_RESULT_USER_CANCELLED,
-                          JOB_RESULT_USER_RESTARTED, JOB_RESULT_TIMEOUT_EXCEEDED)
-JOB_COMPLETE_RESULTS =   (JOB_RESULT_PASSED, JOB_RESULT_SOFTFAILED, JOB_RESULT_FAILED)
-JOB_OK_RESULTS =         (JOB_RESULT_PASSED, JOB_RESULT_SOFTFAILED)
-JOB_INCOMPLETE_RESULTS = (JOB_RESULT_INCOMPLETE, JOB_RESULT_SKIPPED, JOB_RESULT_OBSOLETED,
-                          JOB_RESULT_PARALLEL_FAILED, JOB_RESULT_PARALLEL_RESTARTED,
-                          JOB_RESULT_USER_CANCELLED, JOB_RESULT_USER_RESTARTED,
-                          JOB_RESULT_TIMEOUT_EXCEEDED)
+JOB_RESULTS =              (JOB_RESULT_NONE, JOB_RESULT_PASSED, JOB_RESULT_SOFTFAILED,
+                            JOB_RESULT_FAILED, JOB_RESULT_INCOMPLETE, JOB_RESULT_SKIPPED,
+                            JOB_RESULT_OBSOLETED, JOB_RESULT_PARALLEL_FAILED,
+                            JOB_RESULT_PARALLEL_RESTARTED, JOB_RESULT_USER_CANCELLED,
+                            JOB_RESULT_USER_RESTARTED, JOB_RESULT_TIMEOUT_EXCEEDED)
+JOB_COMPLETE_RESULTS =     (JOB_RESULT_PASSED, JOB_RESULT_SOFTFAILED, JOB_RESULT_FAILED)
+JOB_OK_RESULTS =           (JOB_RESULT_PASSED, JOB_RESULT_SOFTFAILED)
+JOB_NOT_COMPLETE_RESULTS = (JOB_RESULT_INCOMPLETE, JOB_RESULT_TIMEOUT_EXCEEDED)
+JOB_ABORTED_RESULTS =      (JOB_RESULT_SKIPPED, JOB_RESULT_OBSOLETED, JOB_RESULT_PARALLEL_FAILED,
+                            JOB_RESULT_PARALLEL_RESTARTED, JOB_RESULT_USER_CANCELLED,
+                            JOB_RESULT_USER_RESTARTED)
+JOB_NOT_OK_RESULTS =       (JOB_RESULT_FAILED,) + JOB_NOT_COMPLETE_RESULTS + JOB_ABORTED_RESULTS
+
+# 'meta' results
+JOB_RESULT_COMPLETE =     "complete"
+JOB_RESULT_NOT_COMPLETE = "not_complete"
+JOB_RESULT_ABORTED =      "aborted"
 
 # Scenarios
 JOB_SCENARIO_KEYS              = ('DISTRI', 'VERSION', 'FLAVOR', 'ARCH', 'TEST')
