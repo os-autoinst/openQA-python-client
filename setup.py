318cd6ad8125e3248df5d74dae013df8814cae9e
Adam Williamson <awilliam@redhat.com>
2020-02-26 13:44:53 -0800

    Fix long description for pypi
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- setup.py
+++ setup.py
@@ -18,13 +18,14 @@
 import os
 from setuptools import setup, find_packages
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below. Stolen from
-# https://pythonhosted.org/an_example_pypi_project/setuptools.html
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+# From: https://github.com/pypa/pypi-legacy/issues/148
+# Produce rst-formatted long_description if pypandoc is available (to
+# look nice on pypi), otherwise just use the Markdown-formatted one
+try:
+    import pypandoc
+    LONGDESC = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    LONGDESC = open('README.md').read()
 
 setup(
     name = "openqa_client",
@@ -37,7 +38,7 @@ setup(
     url = "https://github.com/os-autoinst/openQA-python-client",
     packages = ["openqa_client"],
     install_requires = ['requests', 'setuptools', 'six'],
-    long_description=read('README.md'),
+    long_description=LONGDESC,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
