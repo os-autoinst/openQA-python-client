c804600ee233fcc6dd35ec62d0a5e5463ed562a7
Adam Williamson <awilliam@redhat.com>
2020-02-28 11:12:33 -0800

    Move source under src/ , fix tox config to run tests on package
    See https://hynek.me/articles/testing-packaging/ , in general.
    We want to build an sdist and run the tests on that, to make
    sure our sdist process is good, we don't want to just run the
    tests on whatever's in the working dir. You can actually do this
    with tox without moving the source under src/ - just removing
    skipsdist from tox.ini does that, tox actually wants to do this
    by default and I've just been cargo-culting that skipsdist around
    for some reason - but if you do that, pytest-cov starts breaking,
    I think on this:
    
    https://github.com/pytest-dev/pytest-cov/issues/134#issuecomment-281883741
    
    moving the source under src/ seems to fix that.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- release.sh
+++ release.sh
@@ -17,8 +17,8 @@ fi
 version=$1
 name=openqa_client
 sed -i -e "s,version=\".*\",version=\"$version\", g" setup.py
-sed -i -e "s,__version__ = \".*\",__version__ = \"${version}\", g" ${name}/__init__.py
-git add setup.py ${name}/__init__.py
+sed -i -e "s,__version__ = \".*\",__version__ = \"${version}\", g" src/${name}/__init__.py
+git add setup.py src/${name}/__init__.py
 git commit -s -m "Release $version"
 git push
 git tag -a -m "Release $version" $version
