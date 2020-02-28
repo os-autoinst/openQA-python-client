94b84454edffe6b6a05b9a6a44996014280ce339
Adam Williamson <awilliam@redhat.com>
2020-02-27 17:54:38 -0800

    Fix more brokenness in setup.py
    Sigh. I really should've tested this.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- setup.py
+++ setup.py
@@ -16,9 +16,17 @@
 # Author: Adam Williamson <awilliam@redhat.com>
 
 from setuptools import setup
+from os import path
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+
+HERE = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     LONGDESC = f.read()
 
 setup(
