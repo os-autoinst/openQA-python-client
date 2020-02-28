f31cfbfbdfca5430e44a76e8b0bb4f0aed970523
Adam Williamson <awilliam@redhat.com>
2020-02-28 12:35:12 -0800

    Update release.sh to use pep517
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- release.sh
+++ release.sh
@@ -2,8 +2,7 @@
 
 baddeps=""
 # check deps
-rpm -qi python3-setuptools > /dev/null 2>&1 || baddeps="python2-setuptools"
-rpm -qi python3-setuptools_git > /dev/null 2>&1 || baddeps="${baddeps} python2-setuptools_git"
+python3 -m pep517.__init__ || baddeps="python3-pep517"
 if [ -n "${baddeps}" ]; then
     echo "${baddeps} must be installed!"
     exit 1
@@ -23,6 +22,5 @@ git commit -s -m "Release $version"
 git push
 git tag -a -m "Release $version" $version
 git push origin $version
-python3 ./setup.py sdist --formats=tar
-gzip dist/$name-$version.tar
-twine upload dist/${name}-${version}.tar.gz
+python3 -m pep517.build .
+twine upload dist/${name}-${version}*
