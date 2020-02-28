971f90e0b57ab7f21df85c7d68dd35089598b14f
Adam Williamson <awilliam@redhat.com>
2020-02-28 11:52:41 -0800

    black-ify code and add black to CI config
    Let's use the shiny new opinionated code formatter everyone's
    talking about:
    
    https://github.com/psf/black
    
    in addition to pylint, for now, because I think pylint may still
    catch stuff black doesn't. This reformats the code and tests to
    pass black - mostly just replacing ' with " - and adds a black
    check to the CI checks. We except const.py because black doesn't
    like the padding spaces we use there, and we set the line length
    to 100 because that's the line length God intended, don't @ me.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,9 +28,11 @@ from unittest import mock
 
 import pytest
 
+
 def _config_teardown(datadir):
     if os.path.exists(datadir):
         shutil.rmtree(datadir)
+
 
 def _config_setup(hosts):
     """Creates a config file in a fake user home directory, at
@@ -39,24 +41,24 @@ def _config_setup(hosts):
     'nokey' in it, in which case we write an entry with no key or
     secret. Before doing this, re-create the home dir.
     """
-    datadir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
-    home = os.path.join(datadir, 'home')
+    datadir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+    home = os.path.join(datadir, "home")
     _config_teardown(datadir)
-    confpath = os.path.join(home, '.config', 'openqa')
+    confpath = os.path.join(home, ".config", "openqa")
     os.makedirs(confpath)
-    confpath = os.path.join(confpath, 'client.conf')
+    confpath = os.path.join(confpath, "client.conf")
     content = []
     for host in hosts:
         if "nokey" in host:
             # don't write a key and secret for this host
             content.extend([f"[{host}]"])
         else:
-            content.extend([f"[{host}]", "key = aaaaaaaaaaaaaaaa",
-                            "secret = bbbbbbbbbbbbbbbb"])
+            content.extend([f"[{host}]", "key = aaaaaaaaaaaaaaaa", "secret = bbbbbbbbbbbbbbbb"])
     content = "\n".join(content)
-    with open(confpath, 'w') as conffh:
+    with open(confpath, "w") as conffh:
         conffh.write(content)
     return (datadir, home)
+
 
 @pytest.fixture(scope="function")
 def config(config_hosts):
@@ -66,9 +68,10 @@ def config(config_hosts):
     completion.
     """
     (datadir, home) = _config_setup(config_hosts)
-    with mock.patch('os.path.expanduser', return_value=home, autospec=True):
+    with mock.patch("os.path.expanduser", return_value=home, autospec=True):
         yield
     _config_teardown(datadir)
+
 
 @pytest.fixture(scope="function")
 def simple_config():
@@ -76,10 +79,11 @@ def simple_config():
     os.path.expanduser to return the home dir, then teardown on test
     completion.
     """
-    (datadir, home) = _config_setup(['openqa.fedoraproject.org'])
-    with mock.patch('os.path.expanduser', return_value=home, autospec=True):
+    (datadir, home) = _config_setup(["openqa.fedoraproject.org"])
+    with mock.patch("os.path.expanduser", return_value=home, autospec=True):
         yield
     _config_teardown(datadir)
+
 
 @pytest.fixture(scope="function")
 def empty_config():
@@ -88,6 +92,6 @@ def empty_config():
     completion.
     """
     (datadir, home) = _config_setup([])
-    with mock.patch('os.path.expanduser', return_value=home, autospec=True):
+    with mock.patch("os.path.expanduser", return_value=home, autospec=True):
         yield
     _config_teardown(datadir)
