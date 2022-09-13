234aeb6ebe19e6c68f9421ce5de383b2453c3306
Adam Williamson <awilliam@redhat.com>
2022-09-13 16:07:08 -0700

    Add project metadata to pyproject.toml
    I'm keeping setup.py for now because the support for this stuff
    is only in very new versions of setuptools (61+), so building
    packages on older Fedora does not work if we drop setup.py. But
    you *can* get rid of setup.py and still build sdists, wheels and
    so on if you have setuptools 61 or newer.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- release.sh
+++ release.sh
@@ -16,8 +16,9 @@ fi
 version=$1
 name=openqa_client
 sed -i -e "s,version=\".*\",version=\"${version}\", g" setup.py
+sed -i -e "s,version = \".*\",version = \"${version}\", g" pyproject.toml
 sed -i -e "s,__version__ = \".*\",__version__ = \"${version}\", g" src/${name}/__init__.py
-git add setup.py src/${name}/__init__.py
+git add setup.py pyproject.toml src/${name}/__init__.py
 git commit -s -m "Release ${version}"
 git push
 git tag -a -m "Release ${version}" ${version}
